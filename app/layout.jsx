17cadb04c5a624e4e8a557a71faaa4d68c97d66e
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-07-02 09:28:22 -0300

    .

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Prompts Creator",
+  description: "Crea y descubre prompts de ChatGPT",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="es">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+
+const Home = () => {
+  return (
+   <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+        Descubre & Crea
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">
+            AI-Powered Prompts
+        </span>
+    </h1>
+    <p className="desc text-center">Prompts creator is an open-source AI prompting tool for modern worlds to discover, create and share creative prompts</p>
+   </section>
+  )
+}
+
+export default Home
